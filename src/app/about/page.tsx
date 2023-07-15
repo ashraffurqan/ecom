f8489eedcaea43847bab721aee540c4b012204e7
ashraffurqan <132605717+ashraffurqan@users.noreply.github.com>
2023-07-15 10:45:15 +0500

    Update page.tsx

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,23 +1,15 @@
 
 const About = () => {
   return (
-    <div className="mt-[100px] mb-[100px]  text-center"> 
+    <div className="mt-[100px] mb-[100px] text-center"> 
 
 
 <div className="text-center mb-10">
-    <h1 className="scroll-m-20 text-xl  tracking-tight lg:text-2xl">
+    <h1 className="scroll-m-20 text-xl tracking-tight lg:text-2xl">
     Asan Mart Online Shopping Mall
     </h1>
   
-    <div className="flex mt-2 justify-center">
-      <div className="w-16 h-1 rounded-full bg-orange-500 inline-flex" />
-    </div>
-  </div>
-
-  <div className="h-[100px] w-fit ml-4 mr-4 bg-gray-500 text-myblack ">
-   
-   
-    </div>
+     </div>
      </div>
   
   )

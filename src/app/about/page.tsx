9824d41e1bc4697a088a7d247e36a0ad4cff5f61
ashraffurqan <132605717+ashraffurqan@users.noreply.github.com>
2023-07-15 10:41:22 +0500

    Update page.tsx

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -16,13 +16,7 @@ const About = () => {
 
   <div className="h-[100px] w-fit ml-4 mr-4 bg-gray-500 text-myblack ">
    
-      <p className="scroll-m-20 text-xl  tracking-tight ">
-    Pakistans first digital hyperlocal shopping mall. We have Everything for Everyone. for real
-    From The Store. To You
-    
-    Our mission is to deliver everything you need to your doorstep. Whether its groceries. medicines. food or apparel. everything is now just a click away. BBMs perspective is to bring the shopping aisles to you. swipe and shop away from the comfort of your homes
-    
-    Dont miss out on the ultimate shopping experience. we are literally bringing the mall to you</p>
+   
     </div>
      </div>
   
